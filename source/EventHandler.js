5149463faff0672452d8d15dfd7c5931debcefc3
deanrobertcook <deanrobertcook@gmail.com>
2014-10-27 23:46:14 +0800

    Remove multiple responsibilities from EventHandler

--- source/EventHandler.js
+++ source/EventHandler.js
@@ -2,6 +2,13 @@ var EventHandler = (function() {
 	var private = {
 		controller: null,
 		keyBuffer: [],
+		logKeyBufferToConsole: function() {
+			for (var i = 0, l = private.keyBuffer.length; i < l; i++) {
+				if(private.keyBuffer[i]) {
+					console.log(i);
+				}
+			}
+		}
 	};
 	
 	function EventHandler (controller) {
@@ -10,62 +17,36 @@ var EventHandler = (function() {
 	};
 
 	EventHandler.prototype = {
-		handleKeyPress: function(event, model) {
+		handleKeyPress: function(event) {
 			//console.log(event);
 			event.preventDefault();
 			event.stopPropagation();
 
 			private.keyBuffer[event.keyCode] = event.type == "keydown";
-
-			//keyLogger
-			for (var i = 0, l = private.keyBuffer.length; i < l; i++) {
-				if(private.keyBuffer[i]) {
-					//console.log(i);
-				}
-			}
-
-			this.modelInFocus = private.sceneBuilder.cameras[0];
-
+			
 			if(private.keyBuffer[67]) { //c
-				var numCameras = this.cameras.length;
-				this.currentCamera += 1;
-				this.currentCamera %= numCameras;
-
-				this.modelInFocus = this.cameras[this.currentCamera];
+				private.controller.receiveUserCommand("nextCam");
 			}
 
 
 			if(private.keyBuffer[65]) { //a
-				this.modelInFocus.translate(-1, 0, 0);
-				console.log("strafeLeft");
+				private.controller.receiveUserCommand("strafeLeft");
 			} if (private.keyBuffer[83]) { //s
-				this.modelInFocus.translate(0, 0, 1);
 			} if (private.keyBuffer[68]) { //d
-				this.modelInFocus.translate(1, 0, 0);
 			} if (private.keyBuffer[81]) { //q
-				this.modelInFocus.translate(0, 1, 0);
 			} if (private.keyBuffer[87]) { //w
-				this.modelInFocus.translate(0, 0, -1);
 			} if (private.keyBuffer[69]) { //e
-				this.modelInFocus.translate(0, -1, 0);
 			} if (private.keyBuffer[38]) { //up
-				this.modelInFocus.rotateX(10);
 			} if (private.keyBuffer[40]) { //down
-				this.modelInFocus.rotateX(-10);
 			} if (private.keyBuffer[37]) { //left
 				if(private.keyBuffer[16]){ //shift
-					this.modelInFocus.rotateZ(10);
 				} else {
-					this.modelInFocus.rotateY(10);
 				}
 			} if (private.keyBuffer[39]) { //right
 				if(private.keyBuffer[16]){ //shift
-					this.modelInFocus.rotateZ(-10);
 				} else {
-					this.modelInFocus.rotateY(-10);
 				}
 			} if (private.keyBuffer[79]) { //o
-				this.modelInFocus.lookAtOrigin(0, 0, 0);
 			}
 		},
 		
